95454044c516380b73b28ae2e8ebdea75bc06489
CarlosPieper <capieper.pieper@gmail.com>
2023-01-19 14:33:08 -0300

    formatting program.cs

--- InGameServices.Api/Program.cs
+++ InGameServices.Api/Program.cs
@@ -12,23 +12,23 @@ var configuration = builder.Configuration;
 
 builder.Services.AddDbContext<InGameServicesDbContext>(options =>
 {
-    options.UseMySql(configuration.GetConnectionString("MySqlConnectionString"), new MySqlServerVersion(new Version(8, 0, 5)));
+  options.UseMySql(configuration.GetConnectionString("MySqlConnectionString"), new MySqlServerVersion(new Version(8, 0, 5)));
 });
 
 var key = configuration["Jwt:Key"];
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
+  .AddJwtBearer(options =>
+  {
+    options.TokenValidationParameters = new TokenValidationParameters
     {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = false,
-            ValidateAudience = false,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
-        };
-    });
+      ValidateIssuer = false,
+      ValidateAudience = false,
+      ValidateLifetime = true,
+      ValidateIssuerSigningKey = true,
+      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
+    };
+  });
 
 builder.Services.AddRepositories();
 builder.Services.AddServices();
@@ -37,38 +37,38 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    {
-        In = ParameterLocation.Header,
-        Description = "Enter token generated on login",
-        Name = "Authorization",
-        BearerFormat = "JWT",
-        Scheme = "Bearer",
-        Type = SecuritySchemeType.Http
-    });
+  options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+  {
+    In = ParameterLocation.Header,
+    Description = "Enter token generated on login",
+    Name = "Authorization",
+    BearerFormat = "JWT",
+    Scheme = "Bearer",
+    Type = SecuritySchemeType.Http
+  });
 
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+  options.AddSecurityRequirement(new OpenApiSecurityRequirement
+  {
     {
+      new OpenApiSecurityScheme
+      {
+        Reference = new OpenApiReference
         {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[]{}
+          Type = ReferenceType.SecurityScheme,
+          Id = "Bearer"
         }
-    });
+      },
+      new string[]{}
+    }
+  });
 });
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
